996044046af09503e1ca84fad3dfdcdf592d1b93
Duncan Band <band.duncan@gmail.com>
2025-04-27 23:32:46 +0100

    fix: remove non-null assertions from Supabase environment variables

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -8,7 +8,7 @@ export function createClient<Database>() {
     throw new Error('Missing Supabase Anon Key');
   }
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   );
 }
