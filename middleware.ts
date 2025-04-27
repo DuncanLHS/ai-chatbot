a97616f7dd149689bf6b58e2538ba867ae61e694
Duncan Band <band.duncan@gmail.com>
2025-04-27 13:16:52 +0100

    chore: formatting

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-import { updateSession } from '@/lib/supabase/middleware'
-import { type NextRequest } from 'next/server'
+import { updateSession } from '@/lib/supabase/middleware';
+import { type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  return await updateSession(request);
 }
 
 export const config = {
@@ -16,4 +16,4 @@ export const config = {
      */
     '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
-}
+};
